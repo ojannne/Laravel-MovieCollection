b6cc22eb82dcd8ad79ea44285c3031c8919c061a
Muhammad Fauzan Nasyiruddin <fauzannashiruddin50@gmail.com>
2025-02-18 07:36:47 +0700

    Delete resources directory

--- resources/views/data_santri.blade.php
+++
@@ -1,5 +0,0 @@
-<h3>Data Santri</h3>
-<ol>
-    <li>Santri 1 : {{ $mhs1 }}, Asal : {{ $asal1 }}</li>
-    <li>Santri 2 : {{ $mhs2 }}, Asal : {{ $asal2 }}</li>
-</ol>
--- resources/views/layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<div style="background: #7AB2D3; font-size: 18px; color: white; text-align: center;">
-    Created by : PeTIK Jombang Software Development @ 2024
-</div>
